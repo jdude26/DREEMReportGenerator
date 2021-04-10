896a6f27c319b787f9c171a8278b14033b85fcc8
Jared Duval <Jduval@ucsc.edu>
2021-04-10 15:30:39 -0700

    before stripping

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const autoprefixer = require('autoprefixer');
+
+    
+module.exports = [{
+  entry: './app.scss',
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+            
+            { loader: 'extract-loader' },
+{ loader: 'css-loader' },
+  {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        plugins: [
+          autoprefixer()
+        ]
+      }
+    } 
+  },
+{
+  loader: 'sass-loader',
+  options: {
+    sassOptions: {
+      includePaths: ['./node_modules']
+    },
+    // Prefer Dart Sass
+    implementation: require('sass'),
+
+    // See https://github.com/webpack-contrib/sass-loader/issues/804
+    webpackImporter: false,
+  }
+},
+          { loader: 'css-loader' },
+          {
+              loader: 'sass-loader',
+              options: {
+                // Prefer Dart Sass
+                implementation: require('sass'),
+
+                // See https://github.com/webpack-contrib/sass-loader/issues/804
+                webpackImporter: false,
+                sassOptions: {
+                  includePaths: ['./node_modules']
+                },
+              }
+            },
+        ]
+      }
+    ]
+  },
+}];
